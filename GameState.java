e5cead73f04c15f2f4dc421aaf534cec5082d60c
Darthveloper21 <hienvungoc01@gmail.com>
2020-09-05 22:45:33 +0700

    Create scene changer

---
+++ GameState.java
@@ -0,0 +1,22 @@
+import java.awt.Graphics;
+
+public class GameState extends State
+{
+    public GameState()
+    {
+
+    }
+
+    @Override
+    public void tick()
+    {
+
+    }
+
+    @Override
+    public void render(Graphics g)
+    {
+        
+    }
+    
+}
